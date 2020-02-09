627c54c10bad39ef3cf7d007601d698c75841a55
Meghna Pradhan <meghna.prd@gmail.com>
2020-02-09 16:57:39 +0530

    updates

--- Application Code/news.py
+++ Application Code/news.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+
+
 
 app= Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///site.db'
 
-#app.config['SQLALCHEMY_DATABASE_URI']='sqlite://'
+db = SQLAlchemy(app)
+
+class Newspost(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    title=db.Column(db.String(50))
+    author = db.Column(db.String(20))
+    content = db.Column(db.Text())
+
+
+
 @app.route('/')
 def index():
    return render_template('index.html')
@@ -15,6 +29,16 @@ def saved():
 def post():
     return render_template('post.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/addpost', methods=['POST'])
+def addpost():
+    title = request.form['title']
+    author = request.form['subtitle']
+    content = request.form['content']
+    return '<h1>Title: {} Author: {} Content: {}</h1>'.format(title,author,content)
 
 if __name__ == '__main__':
     app.run(debug=True)
